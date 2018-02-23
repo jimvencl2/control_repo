896180d594ee8f675edd54d7734d43a9f3eb92cb
jimvencl2 <jim.vencl2@gmail.com>
2018-02-23 10:33:22 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node 'master.puppet.va' {
   include role::master_server
   file { '/root/README':
     ensure => file,
-    content => $fqdn,
+    content => "Welcome to ${fqdn}\n",
   }
 }
 node /^web/ {
