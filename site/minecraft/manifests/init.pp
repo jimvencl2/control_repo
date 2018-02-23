5166e9bd2c34b6e18e976dd1bb1723a1965d713a
jimvencl2 <jim.vencl2@gmail.com>
2018-02-23 13:08:55 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -18,8 +18,11 @@ Class minecraft (
       content => 'eula=true',
    }
    file {'/etc/systemd/system/minecraft.service':
-      ensure => file,
-      source => 'puppet:///modules/minecraft/minecraft.service',
+      ensure  => file,
+      content => epp('minecraft/minecraft.service',{
+          install_dir => $install_dir,
+      })
+      # source => 'puppet:///modules/minecraft/minecraft.service',
     }
    service {'minecraft':
        ensure  => running,
