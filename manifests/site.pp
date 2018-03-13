053707108a6118c03dfbf70a68def6511e63fef7
jimvencl2 <jim.vencl2@gmail.com>
2018-03-13 16:50:44 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node default {
 }
-#node 'master.puppet.va' {
-#  include role::master_server
-#  file { '/root/README':
-#    ensure => file,
-#    content => "Welcome to ${fqdn}\n",
-#  }
-#}
+node 'master.puppet.vm' {
+ include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => "Welcome to ${fqdn}\n",
+  }
+}
 #node 'minetest.puppet.vm' {
 #   include role::minecraft_server
 #}
